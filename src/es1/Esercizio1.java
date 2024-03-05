88f2243b48d24e6d2f4a12d536c66d26553c50c1
Matteo Furgani <151537051+MatteoFurgani@users.noreply.github.com>
2024-03-05 14:44:25 +0100

    Esercizio1 completato

--- src/es1/Esercizio1.java
+++ src/es1/Esercizio1.java
@@ -1,11 +1,28 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Esercizio1 {
     public static void main(String[] args) {
-        String parola = "ciao";
-        System.out.println(stringaPariDispari(parola));
+        Scanner scanner = new Scanner(System.in);
 
+        //----------------------------------1-------------------------
+
+        System.out.println("Inserisci la parola per verificare se è pari o dispari");
+        String parola = scanner.nextLine();
+        System.out.println("pari/dispari: " + stringaPariDispari(parola));
+
+        //----------------------------------2-------------------------
+
+        System.out.println("Inserisci un anno per verificare se è bisestile oppure no");
+        int anno = scanner.nextInt();
+        System.out.println("anno bisestile: " + annoBisestile(anno));
+
+
+
+        scanner.close();
     }
+
 
     public static boolean stringaPariDispari( String parola){
         if(parola.length() % 2 == 0){
@@ -13,6 +30,16 @@ public class Esercizio1 {
         } else {
             return false;
         }
+    }
+
+
+    public static boolean annoBisestile(int anno){
+        if((anno % 4 == 0) && (anno % 100 != 0) || (anno % 400 == 0)){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 }
